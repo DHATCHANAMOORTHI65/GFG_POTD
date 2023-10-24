991bb39b8f75d8ffdfd228be4ed3423e1c0522c5
Chandru <106470788+ChanBotCoder@users.noreply.github.com>
2023-10-24 19:56:14 +0530

    Create isPalindrome.java

---
+++ Hard_Problems/isPalindrome.java
@@ -0,0 +1,31 @@
+class Solution{
+    static boolean isPalindrome(int i,int j,String str){
+        while(i<j){
+            if(str.charAt(i) != str.charAt(j)) return false;
+            i++;
+            j--;
+        }
+        return true;
+    }
+    static int palindromicPartition(String str)
+    {
+        // code here
+        int n = str.length();
+        int dp[] = new int[n+1];
+        dp[n] = 0;
+        
+        for(int i=n-1;i>=0;i--){
+            int minCost = Integer.MAX_VALUE;
+            for(int j=i;j<n;j++){
+                if(isPalindrome(i,j,str)){
+                    int cost = 1 + dp[j+1];
+                    minCost = Math.min(cost,minCost);
+                }
+            }
+            dp[i] = minCost;
+        }
+        return dp[0] - 1;
+    }
+}
+
+// https://practice.geeksforgeeks.org/problems/palindromic-patitioning4845/1
